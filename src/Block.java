552707c84eb2eb850e32378a8070f0ec4f2600f4
Michael Van Leeuwen <michaeljvanleeuwen@gmail.com>
2017-12-21 18:00:25 -0500

    Add data and methods

--- src/Block.java
+++ src/Block.java
@@ -1,2 +1,35 @@
-public class Block {
+import javax.swing.*;
+import java.awt.Color;
+
+public class Block extends JLabel {
+
+    private boolean Active;
+
+    public Block() {
+        setOpaque(true);
+        Active = false;
+    }
+
+    public void setActive(boolean Active) {
+        this.Active = Active;
+    }
+
+    public boolean getActive() {
+        return Active;
+    }
+
+    public void toggleActive() {
+        Active = !Active;
+    }
+
+    public void setColor(Color BlockColor) {
+        setBackground(BlockColor);
+        setActive(true);
+    }
+
+    public void Clear() {
+        setBackground(null);
+        Active = false;
+    }
+
 }
