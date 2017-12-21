80323a2b214ca50e7d6a53b0f5ab8b2ffe3d6b07
Michael Van Leeuwen <michaeljvanleeuwen@gmail.com>
2017-12-21 18:01:00 -0500

    Add data and methods

--- src/GeneratorGUI.java
+++ src/GeneratorGUI.java
@@ -1,2 +1,37 @@
-public class GeneratorGUI {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class GeneratorGUI extends JFrame {
+
+    private JMenuBar mnuBar = new JMenuBar();
+    private JMenu mnuTools = new JMenu("Tools");
+    private JMenuItem mnuRandom = new JMenuItem("Random");
+
+    private BlockGrid myGrid = new BlockGrid();
+
+    public GeneratorGUI() {
+        mnuTools.add(mnuRandom);
+        mnuBar.add(mnuTools);
+        setJMenuBar(mnuBar);
+        mnuRandom.addActionListener(new MenuListener());
+        add(myGrid);
+        setTitle("GitHubPic");
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setSize(500,500);
+        setVisible(true);
+        myGrid.Paint();
+    }
+
+    private class MenuListener implements ActionListener {
+
+        public void actionPerformed(ActionEvent e) {
+            if (e.getSource() == mnuRandom) {
+                myGrid.Clear();
+                myGrid.randColor();
+                myGrid.Paint();
+            }
+        }
+
+    }
 }
