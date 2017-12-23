d240bda259ea86ed5ca815ec563b6a020f77fdc0
Michael Van Leeuwen <michaeljvanleeuwen@gmail.com>
2017-12-23 17:34:15 -0500

    Add image save w/ JFileChooser

--- Profile-Pic-Generator/src/GeneratorGUI.java
+++ Profile-Pic-Generator/src/GeneratorGUI.java
@@ -1,25 +1,31 @@
 import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 
 public class GeneratorGUI extends JFrame {
 
     private JMenuBar mnuBar = new JMenuBar();
     private JMenu mnuTools = new JMenu("Tools");
     private JMenuItem mnuRandom = new JMenuItem("Random");
+    private JMenuItem mnuSave = new JMenuItem("Save");
 
-    private BlockGrid myGrid = new BlockGrid();
+    private BlockGridPanel myGrid = new BlockGridPanel();
 
     public GeneratorGUI() {
         mnuTools.add(mnuRandom);
+        mnuTools.add(mnuSave);
         mnuBar.add(mnuTools);
         setJMenuBar(mnuBar);
         mnuRandom.addActionListener(new MenuListener());
+        mnuSave.addActionListener(new MenuListener());
         add(myGrid);
         setTitle("GitHubPic");
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setSize(500,500);
-        //pack();
+        pack();
         setVisible(true);
         myGrid.Paint();
     }
@@ -31,6 +37,27 @@ public class GeneratorGUI extends JFrame {
                 myGrid.Clear();
                 myGrid.randColor();
                 myGrid.Paint();
+            }
+            else if (e.getSource() == mnuSave) {
+                myGrid.randColor();
+                myGrid.Repaint();
+                JFrame parentFrame = new JFrame();
+                JFileChooser fileChooser = new JFileChooser();
+                fileChooser.addChoosableFileFilter(new FileNameExtensionFilter("PNG Portable Network Graphics Format", ".png"));
+                fileChooser.addChoosableFileFilter(new FileNameExtensionFilter("JPEG File Interchange Format", ".jpg", ".jpeg"));
+                fileChooser.addChoosableFileFilter(new FileNameExtensionFilter("GIF Graphics Interchange Format", ".gif"));
+                fileChooser.setAcceptAllFileFilterUsed(false);
+                fileChooser.setDialogTitle("Save As");
+                int userSelection = fileChooser.showSaveDialog(parentFrame);
+                if (userSelection == JFileChooser.APPROVE_OPTION) {
+                    String Path = fileChooser.getSelectedFile().getAbsolutePath() + "." + fileChooser.getFileFilter().getDescription().substring(0,4).trim().toLowerCase();
+                    if (Path.contains(".") && !Path.contains("jpg") && !Path.contains(".jpeg") && !Path.contains(".png") && !Path.contains(".gif")) {
+                        JOptionPane.showMessageDialog(null,"Error. Please enter a proper file extension (.jpg, jpeg, png, gif).","Extension Error",JOptionPane.WARNING_MESSAGE);
+                    }
+                    else {
+                        myGrid.Save(Path);
+                    }
+                }
             }
         }
 
